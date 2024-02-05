a2869bbb1013d4bb311ec9d9f20c3da9a784366b
RanjitPatil2k01 <156569293+RanjitPatil2k01@users.noreply.github.com>
2024-02-05 23:49:41 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+                <div class="clear"></div>
+            </div>
+            <!-- // #container -->
+        </div>	
+        <!-- // #containerHolder -->
+        
+        <p id="footer" > Bed Management Optimization System. &copy; 2022 Developed & Maintained By <u><b>Ranjit Patil @ 2022</b></u></p>
+    </div>
+    <!-- // #wrapper -->
+</body>
+</html>
